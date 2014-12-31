366078f66329783468cfcb07f460a2808da2cc96
Kelly <452401242@qq.com>
2014-12-31 20:54:05 +0800

    123
    painting function is not finished

--- print_toy.R
+++ print_toy.R
@@ -8,6 +8,8 @@ draw<-function(){
 	color<<-'black'
 	pen<<-1
 	cex<<-5
+	record<<-matrix(0,1e+3,1e+3)
+	flag<<-matrix(0,1e+3,1e+3)
 	add<-function(a,px,py){
 		point<-function(x,y){
 			temp<-matrix(c(runif(30)),ncol=3)
@@ -15,13 +17,33 @@ draw<-function(){
 			temp[,2]<-sin(temp[,3]*2*pi)*0.02*temp[,2]+py-0.01
 			points(temp,pch=16,cex=0.8*cex/10,col=color)
 		}
-			px<-(px-p1[1])/(p2[1]-p1[1])
-			py<-(py-p1[2])/(p2[2]-p1[2])
-			if(px<1 & px>0 & py<1 & py>0){
-				print(color)
-				if(pen==1)points(px,py,pch=16,col=color,cex=cex/10)
-				if(pen==2)point(px,py)
+		px<-round((px-p1[1])/(p2[1]-p1[1]),3)
+		py<-round((py-p1[2])/(p2[2]-p1[2]),3)
+		if(px<1 & px>0 & py<1 & py>0){
+			record[px*(1e+3),py*(1e+3)]<-ifelse(color=='e',0,1)
+			if(pen==1)points(px,py,pch=16,col=color,cex=cex/10)
+			if(pen==2)point(px,py)
+			##if(pen==3)paint(px,py)
+			flag<<-matrix(0,1e+3,1e+3)
+		}
+		return(NULL)
+	}
+	painting<-function(a,px,py){
+		paint<-function(x,y){
+			if(!(x<=1e-3 | y<=1e-3 | x>1 | y>1) & record[x*(1e+3),y*(1e+3)]==record[px*(1e+3),py*(1e+3)] & flag[x*(1e+3),y*(1e+3)]==0){
+				##points(x,y,pch=15,cex=0.1,col=color)
+				flag[x*(1e+3),y*(1e+3)]<<-1
+				if(round(x,1)>(1e-3) & round(y,1)>(1e-3) & round(x,1)<=1-(1e-3) & round(y,1)<=1-(1e-3))if(flag[x*(1e+3),y*(1e+3)+1]==0)paint(x,y+(1e-3))
+				if(round(x,1)>(1e-3) & round(y,1)>(1e-3) & round(x,1)<=1-(1e-3) & round(y,1)<=1-(1e-3))if(flag[x*(1e+3)+1,y*(1e+3)]==0)paint(x+(1e-3),y)
+				if(round(x,1)>(1e-3) & round(y,1)>(1e-3) & round(x,1)<=1-(1e-3) & round(y,1)<=1-(1e-3))if(flag[x*(1e+3),y*(1e+3)-1]==0)paint(x,y-(1e-3))
+				if(round(x,1)>(1e-3) & round(y,1)>(1e-3) & round(x,1)<=1-(1e-3) & round(y,1)<=1-(1e-3))if(flag[x*(1e+3)-1,y*(1e+3)]==0)paint(x-(1e-3),y)
 			}
+		}
+		x<-(px-p1[1])/(p2[1]-p1[1])
+		y<-(py-p1[2])/(p2[2]-p1[2])
+		x<-round(x,1)
+		y<-round(y,1)
+		if(!(x<0 | y<0 | x>1 | y>1) & record[x*(1e+3),y*(1e+3)]==record[px*(1e+3),py*(1e+3)] & flag[x*(1e+3),y*(1e+3)]==0)paint(x,y)
 		return(NULL)
 	}
 	col<-function(K){
@@ -29,12 +51,14 @@ draw<-function(){
 		if(K=='r'){color<<-'red'}
 		if(K=='b'){color<<-'black'}
 		if(K=='g'){color<<-'green'}
-		if(K=='p'){color<<-'pink'}
+		if(K=='p'){color<<-'purple'}
 		if(K=='u'){color<<-'blue'}
 		if(K=='y'){color<<-'yellow'}
-		if(K=='o'){color<<-'organe'}
+		if(K=='o'){color<<-'orange'}
+		if(K=='e'){color<<-'white'}
 		if(K=='q'){pen<<-1}
 		if(K=='w'){pen<<-2}
+		if(K=='a'){pen<<-3}
 		if(K%in%as.character(1:9)){cex<<-as.numeric(K)}
 		points(0,0,col='white')
 	}
